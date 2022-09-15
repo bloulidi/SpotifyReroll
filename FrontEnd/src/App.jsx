99851bb74c977f6eba34e85eb65552d78b313696
Badreddine Loulidi <badred123456@gmail.com>
2022-09-15 14:58:57 -0400

    Add and connect react frontend

---
+++ FrontEnd/src/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+function App() {
+  const [data, setData] = React.useState(null);
+
+  var testAPI = async () => {
+    let res = await fetch("/api")
+    let resJson = await res.json()
+    setData(resJson.message); 
+     };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="container">
+          <h1>This is an example of the Authorization Code flow</h1>
+          <p>{!data ? "Loading..." : data}</p>
+          <a onClick={() => testAPI()} className="btn btn-primary">
+            Log in with Spotify
+          </a>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
